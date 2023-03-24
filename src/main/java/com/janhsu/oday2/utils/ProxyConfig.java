dfe19ae7879f93fc974ab94ead8e3d53b920b22a
d0g3ee <62692103+Janhsu@users.noreply.github.com>
2023-03-25 01:36:23 +0800

    Delete src/main directory

--- src/main/java/com/janhsu/oday2/utils/ProxyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.janhsu.oday2.utils;
-
-public class ProxyConfig {
-    public static String IS_PROXY = "false";
-
-    public static String IP_ADDR = "127.0.0.1";
-
-    public static int PORT_ADDR = 8080;
-}
